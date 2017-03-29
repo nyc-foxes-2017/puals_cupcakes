8b3aa53d89a24c0e81202b88649ddd118901862d
Michael Benson <email": null>
2017-03-29 18:56:39 -0400

    ugly homepage layout

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,4 +2,9 @@ module MoviesHelper
   def average_review_score
     (@movie.reviews.reduce(0){|score, r| score + r.rating } / @movie.reviews.length.to_f).round(1)
   end
+
+  def find_recent_movies
+    recent_reviews = Review.all.order("created_at DESC").limit(5)
+    @movies = recent_reviews.map{ |review| get_movie_by_id( review[:movie_id]) }
+  end
 end
