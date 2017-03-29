989b3e33d6517b27548b2a889abe88a9d81eff8a
Michael Benson <email": null>
2017-03-29 15:23:43 -0400

    reviews mostly complete

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :rating, presence: true, :inclusion => { :in => 1..10 }
+  validates :user_id, uniqueness: {scope: :movie}
 
   def stars
     ('⭐')* self.rating
