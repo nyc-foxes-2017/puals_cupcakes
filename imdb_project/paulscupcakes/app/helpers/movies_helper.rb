4b1748e53085c91705f07794dd88ad63c331e196
Jordany Rosas & Michael Benson & Paul Gallagher <pair+jordanyryan.mabenson00.paulizleet@devbootcamp.com>
2017-03-29 11:46:53 -0400

    Implemented restful routes for Movie controller

--- imdb_project/paulscupcakes/app/helpers/movies_helper.rb
+++ imdb_project/paulscupcakes/app/helpers/movies_helper.rb
@@ -1,2 +1,5 @@
 module MoviesHelper
+  def average_review_score
+    (@movie.reviews.reduce(0){|score, r| score + r.rating } / @movie.reviews.length.to_f).round(1)
+  end
 end
--- imdb_project/paulscupcakes/config/routes.rb
+++ imdb_project/paulscupcakes/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "/movies/search" => "movies#search"
+
   resources :movies do
     resources :reviews
+    resources :comments
   end
+
+
 end
