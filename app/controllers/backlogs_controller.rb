cb3037c99945bb5dedacec8e5e0e73d778cb58ac
Jordany Rosas <email": null>
2017-03-30 11:12:40 -0400

    fixing my machine

--- app/controllers/backlogs_controller.rb
+++ app/controllers/backlogs_controller.rb
@@ -1,2 +1,15 @@
 class BacklogsController < ApplicationController
+
+  def create
+    @backlog = Backlog.new(movie_id: params[:movie_id], user_id: current_user.id)
+    @backlog.save
+    redirect_to "movies#show"
+  end
+
+  def destroy
+    @backlog = Backlog.find_by(movie_id: params[:movie_id], user_id: current_user.id)
+    @backlog.destroy
+    redirect_to "movies#show"
+  end
+
 end
