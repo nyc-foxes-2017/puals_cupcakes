074815e8b7be3c5b3cec5fcd7e97368a39a7fba9
Jordany Rosas <email": null>
2017-03-30 12:04:31 -0400

    Finally got favorite and unfavorite and backlog buttons to work

--- app/controllers/backlogs_controller.rb
+++ app/controllers/backlogs_controller.rb
@@ -1,15 +1,25 @@
 class BacklogsController < ApplicationController
 
+  def index
+    @backlogs = Backlog.all
+  end
+  def new
+    @backlog = Backlog.new
+  end
+
   def create
     @backlog = Backlog.new(movie_id: params[:movie_id], user_id: current_user.id)
-    @backlog.save
-    redirect_to "movies#show"
+    if @backlog.save
+      redirect_to :back
+    else
+      render :new, status: 422
+    end
   end
 
   def destroy
     @backlog = Backlog.find_by(movie_id: params[:movie_id], user_id: current_user.id)
     @backlog.destroy
-    redirect_to "movies#show"
+    redirect_to :back
   end
 
 end
